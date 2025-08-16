14d3712110c0b4ef369f5602072a3de898e91a8b
Jefferson Jalandoon <53508895+bommer153@users.noreply.github.com>
2025-08-16 22:04:32 +0800

    Create services.js
    feat: add api dummy

---
+++ services.js
@@ -0,0 +1,26 @@
+async function insertData(itemData){
+   try{
+   const response = fetch(URL,{
+      header:{
+        content-type:application/json,
+        x-master-key: {key}
+      },
+      body:itemData.json().stringfy;
+   });
+  
+      const data = await response.json(); 
+      return data;  
+   }
+   catch(error){
+      throw Error(error);
+   }
+}
+
+function addItem(item){
+  const itemData = {
+     name : item.name,
+     price : item.price,
+     description : item.description,
+  }
+  insertData(itemData);
+}
